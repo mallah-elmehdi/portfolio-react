05f5ef72a37c3643f81217e412bb9da274bf11d6
mallah-elmehdi <elmehdimallah@gmail.com>
2022-06-08 21:51:07 +0100

    Updates

--- src/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as ColorModeSwitcher } from './ColorModeSwitcher'
-export { default as Header } from './Header'
-export { default as Main } from './Main'
-export { default as Skills } from './Skills'
